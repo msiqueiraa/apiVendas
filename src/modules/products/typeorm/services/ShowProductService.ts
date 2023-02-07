100843958b20fae683d53e191c0355004777bc72
msiqueiraa <matheus.ramosalmeida@hotmail.com>
2023-02-07 15:49:22 -0300

    feat: :sparkles: serviços de buscar um unico produto e atualizar um produto adicionados

---
+++ src/modules/products/typeorm/services/ShowProductService.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/errors/appErros';
+import { getCustomRepository } from 'typeorm';
+import Product from '../entities/Product';
+import { ProductRepository } from '../repositories/ProductsRepository';
+interface IRequest {
+  id: string;
+}
+export default class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
--- src/modules/products/typeorm/services/UpdateProductService.ts
+++ src/modules/products/typeorm/services/UpdateProductService.ts
@@ -2,22 +2,34 @@ import AppError from '@shared/errors/appErros';
 import { getCustomRepository } from 'typeorm';
 import Product from '../entities/Product';
 import { ProductRepository } from '../repositories/ProductsRepository';
-
 interface IRequest {
+  id: string;
   name: string;
   price: number;
   quantity: number;
 }
-
-export default class CreateProductService {
-  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
-    const productExists = await productsRepository.findByName(name);
 
+    const product = await productsRepository.findOne(id);
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    const productExists = await productsRepository.findByName(name);
     if (productExists) {
       throw new AppError('There is already one product whit this name');
     }
-    const product = productsRepository.create({ name, price, quantity });
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
 
     await productsRepository.save(product);
 
